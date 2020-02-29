05861d99181e56fc34e5ade0bacf564e835c3263
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-02-29 11:50:38 +0100

    BoardConfig.mk: remove qcom-specific flag

--- start.sh
+++ start.sh
@@ -419,7 +419,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := \"hbtp_vm\"
 TW_USE_TOOLBOX := true" >> BoardConfig.mk
