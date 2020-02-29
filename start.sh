930f4044ea6b437a206e4f963c9c69732671c44a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-02-29 09:47:09 +0100

    BoardConfig.mk: add support to Samsung Odin tar and SEAndroid status
    * Thanks to TwrpBuilder for the Makefile

--- start.sh
+++ start.sh
@@ -358,6 +358,11 @@ BOARD_MKBOOTIMG_ARGS := --dt device/$DEVICE_MANUFACTURER/$DEVICE_CODENAME/prebui
 		echo "TARGET_PREBUILT_KERNEL := device/$DEVICE_MANUFACTURER/$DEVICE_CODENAME/prebuilt/zImage-dtb" >> BoardConfig.mk
 fi
 
+if [ "$DEVICE_MANUFACTURER" = "samsung" ]
+	then
+		echo "BOARD_CUSTOM_BOOTIMG_MK := \$LOCAL_PATH/mkbootimg.mk" >> BoardConfig.mk
+fi
+
 # Add LZMA compression if kernel suppport it
 case $RAMDISKCOMPRESSION in
 	lzma)
@@ -475,6 +480,28 @@ if [ $DEVICE_IS_SAR = 1 ]
 	then
 		echo "on fs
 	export ANDROID_ROOT /system_root" >> recovery/root/init.recovery.sar.rc
+fi
+
+# If this is a Samsung device, add support to SEAndroid status and make an Odin-flashable tar
+if [ "$DEVICE_MANUFACTURER" = "samsung" ]
+	then
+		echo "LOCAL_PATH := \$(call my-dir)
+
+\$(INSTALLED_BOOTIMAGE_TARGET): \$(MKBOOTIMG) \$(INTERNAL_BOOTIMAGE_FILES)
+	\$(call pretty,\"Target boot image: \$@\")
+	\$(hide) \$(MKBOOTIMG) \$(INTERNAL_BOOTIMAGE_ARGS) \$(BOARD_MKBOOTIMG_ARGS) --output \$@
+	@echo -e \${CL_CYN}\"Made boot image: \$@\"\${CL_RST}
+
+\$(INSTALLED_RECOVERYIMAGE_TARGET): \$(MKBOOTIMG) \
+		\$(recovery_ramdisk) \
+		\$(recovery_kernel)
+	@echo -e \${CL_CYN}\"----- Making recovery image ------\"\${CL_RST}
+	\$(hide) \$(MKBOOTIMG) \$(INTERNAL_RECOVERYIMAGE_ARGS) \$(BOARD_MKBOOTIMG_ARGS) --output \$@
+	@echo -e \${CL_CYN}\"Made recovery image: \$@\"\${CL_RST}
+	@echo -e \${CL_GRN}\"----- Lying about SEAndroid state to Samsung bootloader ------\"\${CL_RST}
+	\$(hide) echo -n \"SEANDROIDENFORCE\" >> \$(INSTALLED_RECOVERYIMAGE_TARGET)
+	\$(hide) \$(call assert-max-image-size,\$@,\$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+" >> mkbootimg.mk
 fi
 
 # Automatically create a ready-to-push repo
