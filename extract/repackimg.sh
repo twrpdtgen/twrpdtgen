79dcd9def1c53626ef63fb9e0fa446fa56b8e165
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-03 13:22:02 +0200

    repackimg: lint

--- extract/repackimg.sh
+++ extract/repackimg.sh
@@ -15,7 +15,7 @@ case $(uname -s) in
   ;;
   *) plat="linux";;
 esac;
-arch=$plat/`uname -m`;
+arch="$plat/$(uname -m)";
 
 aik="${BASH_SOURCE:-$0}";
 aik="$(dirname "$(readlink -f "$aik")")";
@@ -31,8 +31,8 @@ case $plat in
     xz() { DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/xz" "$@"; }
 
     javaver=$(java -version 2>&1 | head -n1 | cut -d\" -f2);
-    javamaj=$(echo $javaver | cut -d. -f1);
-    javamin=$(echo $javaver | cut -d. -f2);
+    javamaj=$(echo "$javaver" | cut -d. -f1);
+    javamin=$(echo "$javaver" | cut -d. -f2);
     if [ "$javamaj" -lt 9 ] && [ "$javamaj" -eq 1 -a "$javamin" -lt 8 ]; then
       java() { "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java" "$@"; }
     fi;
@@ -69,7 +69,7 @@ while [ "$1" ]; do
     --avbkey)
       if [ "$2" ]; then
         for keytest in "$2" "$cur/$2" "$aik/$2"; do
-          if [ ! -z "$(ls $keytest.pk8 2>/dev/null)" -a ! -z "$(ls $keytest.x509.* 2>/dev/null)" ]; then
+          if [ ! -z "$(ls "$keytest.pk8" 2>/dev/null)" -a ! -z "$(ls "$keytest.x509.*" 2>/dev/null)" ]; then
             avbkey="$keytest"; avbtxt=" - Key: $2"; shift; break;
           fi;
         done;
@@ -79,7 +79,7 @@ while [ "$1" ]; do
   shift;
 done;
 
-ramdiskcomp=`cat split_img/*-ramdiskcomp 2>/dev/null`;
+ramdiskcomp=$(cat split_img/*-ramdiskcomp 2>/dev/null);
 if [ -z "$(ls ramdisk/* 2>/dev/null)" ] && [ ! "$ramdiskcomp" = "empty" -a ! "$original" ]; then
   echo "No files found to be packed/built.";
   abort;
@@ -92,7 +92,7 @@ echo "Android Image Kitchen - RepackImg Script";
 echo "by osm0sis @ xda-developers";
 echo " ";
 
-if [ ! -z "$(ls *-new.* 2>/dev/null)" ]; then
+if [ ! -z "$(ls ./*-new.* 2>/dev/null)" ]; then
   echo "Warning: Overwriting existing files!";
   echo " ";
 fi;
@@ -131,7 +131,7 @@ else
     compext=.$compext;
   fi;
   cd ramdisk;
-  $sudo find . | $sudo $cpio $cpioarg -H newc -o 2>/dev/null | $repackcmd > ../ramdisk-new.cpio$compext;
+  $sudo find . | $sudo $cpio $cpioarg -H newc -o 2>/dev/null | $repackcmd > "../ramdisk-new.cpio$compext";
   if [ ! $? -eq "0" ]; then
     abort;
     exit 1;
@@ -142,88 +142,88 @@ fi;
 echo " ";
 echo "Getting build information...";
 cd split_img;
-imgtype=`cat *-imgtype`;
-if [ "$imgtype" != "KRNL" -a -f *-zImage ]; then
-  kernel=`ls *-zImage`;                 echo "kernel = $kernel";
+imgtype=$(cat ./*-imgtype);
+if [ "$imgtype" != "KRNL" -a -f ./*-zImage ]; then
+  kernel=$(ls ./*-zImage);                 echo "kernel = $kernel";
   kernel="split_img/$kernel";
 fi;
 if [ "$original" ]; then
-  ramdisk=`ls *-ramdisk.cpio*`;         echo "ramdisk = $ramdisk";
+  ramdisk=$(ls ./*-ramdisk.cpio*);         echo "ramdisk = $ramdisk";
   ramdisk="split_img/$ramdisk";
 else
   ramdisk="ramdisk-new.cpio$compext";
 fi;
 case $imgtype in
   KRNL) rsz=$(wc -c < ../"$ramdisk");   echo "ramdisk_size = $rsz";;
-  OSIP)                                 echo "cmdline = $(cat *-cmdline)";;
+  OSIP)                                 echo "cmdline = $(cat ./*-cmdline)";;
   U-Boot)
-    name=`cat *-name`;                  echo "name = $name";
-    uarch=`cat *-arch`;
-    os=`cat *-os`;
-    type=`cat *-type`;
-    comp=`cat *-comp`;                  echo "type = $uarch $os $type ($comp)";
+    name=$(cat ./*-name);                  echo "name = $name";
+    uarch=$(cat ./*-arch);
+    os=$(cat ./*-os);
+    type=$(cat ./*-type);
+    comp=$(cat ./*-comp);                  echo "type = $uarch $os $type ($comp)";
     test "$comp" = "uncompressed" && comp=none;
-    addr=`cat *-addr`;                  echo "load_addr = $addr";
-    ep=`cat *-ep`;                      echo "entry_point = $ep";
+    addr=$(cat ./*-addr);                  echo "load_addr = $addr";
+    ep=$(cat ./*-ep);                      echo "entry_point = $ep";
   ;;
   *)
     if [ -f *-second ]; then
-      second=`ls *-second`;             echo "second = $second";
+      second=$(ls ./*-second);             echo "second = $second";
       second=(--second "split_img/$second");
     fi;
     if [ -f *-dtb ]; then
-      dtb=`ls *-dtb`;                   echo "dtb = $dtb";
+      dtb=$(ls ./*-dtb);                   echo "dtb = $dtb";
       dtb=(--dtb "split_img/$dtb");
     fi;
-    if [ -f *-recoverydtbo ]; then
-      recoverydtbo=`ls *-recoverydtbo`; echo "recovery_dtbo = $recoverydtbo";
+    if [ -f ./*-recoverydtbo ]; then
+      recoverydtbo=$(ls ./*-recoverydtbo); echo "recovery_dtbo = $recoverydtbo";
       recoverydtbo=(--recovery_dtbo "split_img/$recoverydtbo");
     fi;
-    if [ -f *-cmdline ]; then
-      cmdname=`ls *-cmdline`;
-      cmdline=`cat *-cmdline`;          echo "cmdline = $cmdline";
+    if [ -f ./*-cmdline ]; then
+      cmdname=$(ls ./*-cmdline);
+      cmdline=$(cat ./*-cmdline);          echo "cmdline = $cmdline";
       cmd=("split_img/$cmdname"@cmdline);
     fi;
-    if [ -f *-board ]; then
-      board=`cat *-board`;              echo "board = $board";
+    if [ -f ./*-board ]; then
+      board=$(cat ./*-board);              echo "board = $board";
     fi;
-    base=`cat *-base`;                  echo "base = $base";
-    pagesize=`cat *-pagesize`;          echo "pagesize = $pagesize";
-    kerneloff=`cat *-kerneloff`;        echo "kernel_offset = $kerneloff";
-    ramdiskoff=`cat *-ramdiskoff`;      echo "ramdisk_offset = $ramdiskoff";
+    base=$(cat ./*-base);                  echo "base = $base";
+    pagesize=$(cat ./*-pagesize);          echo "pagesize = $pagesize";
+    kerneloff=$(cat ./*-kerneloff);        echo "kernel_offset = $kerneloff";
+    ramdiskoff=$(cat ./*-ramdiskoff);      echo "ramdisk_offset = $ramdiskoff";
     if [ -f *-secondoff ]; then
-      secondoff=`cat *-secondoff`;      echo "second_offset = $secondoff";
+      secondoff=$(cat ./*-secondoff);      echo "second_offset = $secondoff";
     fi;
-    if [ -f *-tagsoff ]; then
-      tagsoff=`cat *-tagsoff`;          echo "tags_offset = $tagsoff";
+    if [ -f ./*-tagsoff ]; then
+      tagsoff=$(cat ./*-tagsoff);          echo "tags_offset = $tagsoff";
     fi;
-    if [ -f *-dtboff ]; then
-      dtboff=`cat *-dtboff`;          echo "dtb_offset = $dtboff";
+    if [ -f ./*-dtboff ]; then
+      dtboff=$(cat ./*-dtboff);          echo "dtb_offset = $dtboff";
     fi;
-    if [ -f *-osversion ]; then
-      osver=`cat *-osversion`;          echo "os_version = $osver";
+    if [ -f ./*-osversion ]; then
+      osver=$(cat ./*-osversion);          echo "os_version = $osver";
     fi;
-    if [ -f *-oslevel ]; then
-      oslvl=`cat *-oslevel`;            echo "os_patch_level = $oslvl";
+    if [ -f ./*-oslevel ]; then
+      oslvl=$(cat ./*-oslevel);            echo "os_patch_level = $oslvl";
     fi;
-    if [ -f *-headerversion ]; then
-      hdrver=`cat *-headerversion`;     echo "header_version = $hdrver";
+    if [ -f -/*-headerversion ]; then
+      hdrver=$(cat ./*-headerversion);     echo "header_version = $hdrver";
     fi;
-    if [ -f *-hash ]; then
-      hash=`cat *-hash`;                echo "hash = $hash";
+    if [ -f ./*-hash ]; then
+      hash=$(cat ./*-hash);                echo "hash = $hash";
       hash="--hash $hash";
     fi;
-    if [ -f *-dt ]; then
-      dttype=`cat *-dttype`;
-      dt=`ls *-dt`;                     echo "dt = $dt";
+    if [ -f ./*-dt ]; then
+      dttype=$(cat ./*-dttype);
+      dt=$(ls ./*-dt);                     echo "dt = $dt";
       rpm=("split_img/$dt",rpm);
       dt=(--dt "split_img/$dt");
     fi;
-    if [ -f *-unknown ]; then
-      unknown=`cat *-unknown`;          echo "unknown = $unknown";
+    if [ -f ./*-unknown ]; then
+      unknown=$(cat ./*-unknown);          echo "unknown = $unknown";
     fi;
-    if [ -f *-header ]; then
-      header=`ls *-header`;
+    if [ -f ./*-header ]; then
+      header=$(ls ./*-header);
       header="split_img/$header";
     fi;
   ;;
@@ -231,18 +231,18 @@ esac;
 cd ..;
 
 if [ -f split_img/*-mtktype ]; then
-  mtktype=`cat split_img/*-mtktype`;
+  mtktype=$(cat split_img/*-mtktype);
   echo " ";
   echo "Generating MTK headers...";
   echo " ";
   echo "Using ramdisk type: $mtktype";
-  "$bin/$arch/mkmtkhdr" --kernel "$kernel" --$mtktype "$ramdisk" >/dev/null;
+  "$bin/$arch/mkmtkhdr" --kernel "$kernel" "--$mtktype" "$ramdisk" >/dev/null;
   if [ ! $? -eq "0" ]; then
     abort;
     exit 1;
   fi;
   mv -f "$(basename "$kernel")-mtk" kernel-new.mtk;
-  mv -f "$(basename "$ramdisk")-mtk" $mtktype-new.mtk;
+  mv -f "$(basename "$ramdisk")-mtk" "$mtktype-new.mtk";
   kernel=kernel-new.mtk;
   ramdisk=$mtktype-new.mtk;
 fi;
@@ -266,8 +266,8 @@ echo " ";
 echo "Using format: $imgtype";
 echo " ";
 case $imgtype in
-  AOSP) "$bin/$arch/mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" "${dtb[@]}" "${recoverydtbo[@]}" --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --second_offset "$secondoff" --tags_offset "$tagsoff" --dtb_offset "$dtboff" --os_version "$osver" --os_patch_level "$oslvl" --header_version "$hdrver" $hash "${dt[@]}" -o $outname;;
-  AOSP-PXA) "$bin/$arch/pxa-mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --second_offset "$secondoff" --tags_offset "$tagsoff" --unknown $unknown "${dt[@]}" -o $outname;;
+  AOSP) "$bin/$arch/mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" "${dtb[@]}" "${recoverydtbo[@]}" --cmdline "$cmdline" --board "$board" --base "$base" --pagesize "$pagesize" --kernel_offset "$kerneloff" --ramdisk_offset "$ramdiskoff" --second_offset "$secondoff" --tags_offset "$tagsoff" --dtb_offset "$dtboff" --os_version "$osver" --os_patch_level "$oslvl" --header_version "$hdrver" $hash "${dt[@]}" -o "$outname";;
+  AOSP-PXA) "$bin/$arch/pxa-mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" --cmdline "$cmdline" --board "$board" --base "$base" --pagesize "$pagesize" --kernel_offset "$kerneloff" --ramdisk_offset "$ramdiskoff" --second_offset "$secondoff" --tags_offset "$tagsoff" --unknown "$unknown" "${dt[@]}" -o "$outname";;
   ELF) "$bin/$arch/elftool" pack -o $outname header="$header" "$kernel" "$ramdisk",ramdisk "${rpm[@]}" "${cmd[@]}" >/dev/null;;
   KRNL) "$bin/$arch/rkcrc" -k "$ramdisk" $outname;;
   OSIP)
@@ -284,7 +284,7 @@ case $imgtype in
       Multi) part1=(:"$ramdisk");;
       RAMDisk) part0="$ramdisk";;
     esac;
-    "$bin/$arch/mkimage" -A $uarch -O $os -T $type -C $comp -a $addr -e $ep -n "$name" -d "$part0""${part1[@]}" $outname >/dev/null;
+    "$bin/$arch/mkimage" -A "$uarch" -O "$os" -T "$type" -C "$comp" -a "$addr" -e "$ep" -n "$name" -d "$part0""${part1[@]}" "$outname" >/dev/null;
   ;;
   *) echo " "; echo "Unsupported format."; abort; exit 1;;
 esac;
@@ -296,12 +296,12 @@ fi;
 rm -rf split_img/.temp;
 
 if [ -f split_img/*-sigtype ]; then
-  sigtype=`cat split_img/*-sigtype`;
+  sigtype=$(cat split_img/*-sigtype);
   if [ -f split_img/*-avbtype ]; then
-    avbtype=`cat split_img/*-avbtype`;
+    avbtype=$(cat split_img/*-avbtype);
   fi;
   if [ -f split_img/*-blobtype ]; then
-    blobtype=`cat split_img/*-blobtype`;
+    blobtype=$(cat split_img/*-blobtype);
   fi;
   echo "Signing new image...";
   echo " ";
@@ -309,10 +309,10 @@ if [ -f split_img/*-sigtype ]; then
   test ! "$avbkey" && avbkey="$bin/avb/verity";
   echo " ";
   case $sigtype in
-    AVBv1) java -jar "$bin/BootSignature.jar" /$avbtype unsigned-new.img "$avbkey.pk8" "$avbkey.x509."* image-new.img 2>/dev/null;;
+    AVBv1) java -jar "$bin/BootSignature.jar" "/$avbtype" unsigned-new.img "$avbkey.pk8" "$avbkey.x509."* image-new.img 2>/dev/null;;
     BLOB)
       awk 'BEGIN { printf "-SIGNED-BY-SIGNBLOB-\00\00\00\00\00\00\00\00" }' > image-new.img;
-      "$bin/$arch/blobpack" tempblob $blobtype unsigned-new.img >/dev/null;
+      "$bin/$arch/blobpack" tempblob "$blobtype" unsigned-new.img >/dev/null;
       cat tempblob >> image-new.img;
       rm -rf tempblob;
     ;;
@@ -330,14 +330,14 @@ if [ -f split_img/*-sigtype ]; then
 fi;
 
 if [ -f split_img/*-lokitype ]; then
-  lokitype=`cat split_img/*-lokitype`;
+  lokitype=$(cat split_img/*-lokitype);
   echo "Loki patching new image...";
   echo " ";
   echo "Using type: $lokitype";
   echo " ";
   mv -f image-new.img unlokied-new.img;
   if [ -f aboot.img ]; then
-    "$bin/$arch/loki_tool" patch $lokitype aboot.img unlokied-new.img image-new.img >/dev/null;
+    "$bin/$arch/loki_tool" patch "$lokitype" aboot.img unlokied-new.img image-new.img >/dev/null;
     if [ ! $? -eq "0" ]; then
       echo "Patching failed.";
       abort;
@@ -351,7 +351,7 @@ if [ -f split_img/*-lokitype ]; then
 fi;
 
 if [ -f split_img/*-tailtype ]; then
-  tailtype=`cat split_img/*-tailtype`;
+  tailtype=$(cat split_img/*-tailtype);
   echo "Appending footer...";
   echo " ";
   echo "Using type: $tailtype";
