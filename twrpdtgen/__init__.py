601b8d18f50657ba8f6717461e6b9e91bd7ce621
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-02 02:39:12 +0100

    templates: Move inside the module

--- twrpdtgen/__init__.py
+++ twrpdtgen/__init__.py
@@ -3,8 +3,9 @@ from jinja2 import Environment, FileSystemLoader
 
 __version__ = "1.1.0"
 
-current_path = Path(__file__).parent.parent
+module_path = Path(__file__).parent
+current_path = module_path.parent
 aik_path = current_path / "extract"
 
-jinja_env = Environment(loader=FileSystemLoader(current_path / 'templates'),
+jinja_env = Environment(loader=FileSystemLoader(module_path / 'templates'),
                         autoescape=True, trim_blocks=True, lstrip_blocks=True)
