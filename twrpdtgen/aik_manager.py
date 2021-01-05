b6b5722fca16583c5e4ce2f4c120590efa4e3f38
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-06 00:07:46 +0100

    aik_manager: _path -> path

--- twrpdtgen/aik_manager.py
+++ twrpdtgen/aik_manager.py
@@ -28,17 +28,17 @@ class AIKManager:
 		self.is_debug = is_debug
 		if not self.is_debug:
 			self.tempdir = TemporaryDirectory()
-			self._path = Path(self.tempdir.name)
+			self.path = Path(self.tempdir.name)
 		else:
-			self._path = current_path / "extract"
-		if self._path.is_dir():
-			rmtree(self._path, ignore_errors=False, onerror=handle_remove_readonly)
+			self.path = current_path / "extract"
+		if self.path.is_dir():
+			rmtree(self.path, ignore_errors=False, onerror=handle_remove_readonly)
 
 		info("Cloning AIK...")
 		if system() == "Linux":
-			Repo.clone_from("https://github.com/SebaUbuntu/AIK-Linux-mirror", self._path)
+			Repo.clone_from("https://github.com/SebaUbuntu/AIK-Linux-mirror", self.path)
 		elif system() == "Windows":
-			Repo.clone_from("https://github.com/SebaUbuntu/AIK-Windows-mirror", self._path)
+			Repo.clone_from("https://github.com/SebaUbuntu/AIK-Windows-mirror", self.path)
 
 	def extract(self, recovery_image: Union[Path, str]) -> Tuple[Path, Path]:
 		"""
@@ -46,18 +46,18 @@ class AIKManager:
 		:param recovery_image: recovery image string or path object
 		:return: extracted ramdisk and split image tuple of path objects
 		"""
-		new_recovery_image = self._path / "recovery.img"
+		new_recovery_image = self.path / "recovery.img"
 		copyfile(recovery_image, new_recovery_image)
 		# TODO Check if AIK extract is successful
 		if system() == "Linux":
-			aik_process = Popen([self._path / "unpackimg.sh", "--nosudo", new_recovery_image],
+			aik_process = Popen([self.path / "unpackimg.sh", "--nosudo", new_recovery_image],
 								stdout=PIPE, stderr=PIPE, universal_newlines=True)
 			_, _ = aik_process.communicate()
 		elif system() == "Windows":
-			call([self._path / "unpackimg.bat", new_recovery_image])
+			call([self.path / "unpackimg.bat", new_recovery_image])
 		else:
 			raise NotImplementedError(f"{system()} is not supported!")
-		return self._path / "ramdisk", self._path / "split_img"
+		return self.path / "ramdisk", self.path / "split_img"
 
 	def cleanup(self):
 		if not self.is_debug:
