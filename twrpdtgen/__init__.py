730f0746827aee555fb94fee6048aa54db539a5b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-06 02:02:32 +0100

    treewide: 1.2.0

--- twrpdtgen/__init__.py
+++ twrpdtgen/__init__.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 module_path = Path(__file__).parent
 current_path = Path(os.getcwd())
