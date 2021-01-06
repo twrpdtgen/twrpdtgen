9148c8f948e79f4efd6be14f9e3be2d9a2eee7c8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-06 01:59:16 +0100

    logging: Remove date and time from debug mode
    * We don't need it

--- twrpdtgen/utils/logging.py
+++ twrpdtgen/utils/logging.py
@@ -2,7 +2,7 @@ from logging import basicConfig, INFO, DEBUG
 
 def setup_logging(verbose=False):
 	if verbose:
-		basicConfig(format='[%(asctime)s] [%(filename)s:%(lineno)s %(levelname)s] %(funcName)s: %(message)s',
+		basicConfig(format='[%(filename)s:%(lineno)s %(levelname)s] %(funcName)s: %(message)s',
 					level=DEBUG)
 	else:
 		basicConfig(format='[%(levelname)s] %(message)s', level=INFO)
